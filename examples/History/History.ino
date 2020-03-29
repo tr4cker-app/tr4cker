6f7bf34bee4ddf054ba935c11a523c71c28a585b
Robert Sasak <robert.sasak@gmail.com>
2020-03-29 20:49:16 +0200

    Update example

--- examples/History/History.ino
+++ examples/History/History.ino
@@ -2,16 +2,20 @@
 
 Tr4cker tr4cker("put-your-device-key-here");
 
+#define INTERVAL 15
+
 void setup() {
     Serial.begin(115200);
 }
 
 void loop() {
     tr4cker.setBatteryPin(A0);
-    tr4cker.enableHistory(true);
+    // We do not have access to real real date and time. 
+    // So instead of storing actual time, tr4acker need to know how often do we perform location tracking. 
+    // enableHistory takes integer representig measuring interval in minutes.
+    // This have to be the same 
+    tr4cker.enableHistory(INTERVAL); // in minutes
     tr4cker.begin();
     
-    // Sleep for 15 minutes
-    Serial.println("Going to sleep.");
-    delay(1*60*1000);
+    ESP.deepSleep(INTERVAL * 60 * 1000 * 1000);
 }
