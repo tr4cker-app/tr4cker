b3c789a34fcefa88c621a4760de0349f8a58f759
Robert Sasak <robert.sasak@gmail.com>
2020-03-30 17:51:24 +0200

    Typo

--- examples/History/History.ino
+++ examples/History/History.ino
@@ -13,9 +13,10 @@ void loop() {
     // We do not have access to real real date and time. 
     // So instead of storing actual time, tr4acker need to know how often do we perform location tracking. 
     // enableHistory takes integer representig measuring interval in minutes.
-    // This have to be the same 
+    // This have to be the same as deep sleep interval
     tr4cker.enableHistory(INTERVAL); // in minutes
     tr4cker.begin();
     
+    Serial.println("Going to deep sleep");
     ESP.deepSleep(INTERVAL * 60 * 1000 * 1000);
 }
